be3d92be01634bd42fd96ec6324b97069b72f92e
CHANCHAO\Anthony <anthony@chanchao.com.tw>
2023-10-31 15:59:02 +0800

    修正web.confg及server/index.js及加入api/index.js

---
+++ api/index.js
@@ -0,0 +1,10 @@
+var router = require('express').Router()
+
+// Add USERS Routes
+//router.use(require('./index'))
+// router.get('/index', async (req, res) => {
+//   let html = await nuxt.renderRoute('/index', context)
+//   return res.send(html)
+// })
+
+module.exports = router
